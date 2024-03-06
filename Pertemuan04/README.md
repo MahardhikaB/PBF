7b71500fa03ef0da85d0aab8b94ccaa1a97d393f
Mahardhika Bredy Dwi Gagah Santoso <bredymahardhika@gmail.com>
2024-03-06 08:49:39 +0700

    Praktikum 01

---
+++ Pertemuan04/README.md
@@ -0,0 +1,22 @@
+# Pertemuan 04 : Event dan State
+
+1. **Praktikum 1 - Langkah 1**
+
+    Kode :
+
+    <img src="images/P1_Langkah1.png" alt="Alt Text" width="500"/>
+
+    Hasil :
+
+    <img src="images/P1_Hasil1.png" alt="Alt Text" width="500"/>
+
+2. **Praktikum 1 - Langkah 2**
+
+    Kode :
+
+    <img src="images/P1_Langkah2.png" alt="Alt Text" width="500"/>
+
+    Hasil :
+
+    <img src="images/P1_Hasil2.png" alt="Alt Text" width="500"/>
+    
---
+++ Pertemuan04/src/components/button.tsx
@@ -0,0 +1,22 @@
+"use client";
+export default function Tombol_1() {
+    // Menambah fungsi untuk menangani klik tombol
+    function handleClick() {
+        alert('Tombol telah ditekan!');
+    }
+
+    function handleMouseOver() {
+        console.log('Eits, mau mencet tombol ya?');
+    }
+
+    return (
+        <button 
+            className="bg-blue-500 hover:bg-blue-700 text-white p-2 rounded"
+            onClick={handleClick}
+            onMouseOver={handleMouseOver}
+        >
+            Ini Tombol
+        </button>
+    )
+}
+
