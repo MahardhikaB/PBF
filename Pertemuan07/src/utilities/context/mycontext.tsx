786d90cc30fdada8ecf92d4616e17d6b2b4de05c
Mahardhika Bredy Dwi Gagah Santoso <bredymahardhika@gmail.com>
2024-04-16 20:47:39 +0700

    Initial Commit

--- Pertemuan07/src/utilities/context/mycontext.tsx
+++ Pertemuan07/src/utilities/context/mycontext.tsx
@@ -1,3 +1,10 @@
 import { createContext } from "react";
+import { themes } from "../themes/mythemes";
 
-export const levelContext = createContext(1);
+export const levelContext = createContext(0);
+
+export const ThemeContext = createContext({
+    theme: themes.light,
+    toggleTheme: () => {}
+})
+
