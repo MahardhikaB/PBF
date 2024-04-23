6cc439cdd9f4f4dcf4144cc2146831d3dc27309f
Mahardhika Bredy Dwi Gagah Santoso <bredymahardhika@gmail.com>
2024-04-23 23:30:10 +0700

    Add Modal

--- uts-pbf/src/app/data_user/page.tsx
+++ uts-pbf/src/app/data_user/page.tsx
@@ -3,6 +3,7 @@ import DashTemplate from "../dash_template";
 import { Button, Checkbox, Label, Modal, TextInput } from "flowbite-react";
 import Link from "next/link";
 import { useRef, useState } from "react";
+import { IoClose } from "react-icons/io5";
 
 export default function DataUser() {
     const [openModal, setOpenModal] = useState(false);
@@ -55,33 +56,56 @@ export default function DataUser() {
             </tbody>
             </table>
         </div>
-        <Modal show={openModal} size="md" className="bg-black bg-opacity-75" onClose={() => setOpenModal(false)} popup>
-            <Modal.Header/>
+        <Modal
+            show={openModal}
+            size="md"
+            className="bg-black bg-opacity-75"
+            onClose={() => setOpenModal(false)}
+            popup
+        >
+            <Modal.Header />
             <Modal.Body className="mx-20">
-            <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-100 w-1/2 rounded-2xl bg-white flex flex-col text-black'>
-                <h1 className='p-3 m-3 font-bold text-2xl'>Tambah User</h1>
-                <div className='h-1 w-full bg-white'/>
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-100 w-1/2 rounded-2xl bg-white flex flex-col text-black py-3">
+                <div className="flex flex-row items-center justify-between px-3 mx-3 mb-5">
+                <h1 className="font-bold text-2xl">Tambah User</h1>
+                <IoClose size={38} onClick={() => setOpenModal(false)} />
+                </div>
                 <h1 className="pl-16 font-bold">User ID</h1>
                 <div className="flex flex-row mr-16">
-                <input type="text" className='ml-16 mr-3 mt-1 mb-2 w-full rounded-2xl bg-white py-3 font-bold focus:border-black' />
+                <input
+                    type="text"
+                    className="ml-16 mr-3 mt-1 mb-2 w-full rounded-2xl bg-white py-3 font-bold focus:border-black"
+                />
                 <Button className="mt-1 mb-2 rounded-xl bg-black text-white font-semibold p-1 items-center">
-                Generate
+                    Generate
                 </Button>
                 </div>
                 <h1 className="pl-16 pt-1 font-bold">Nama</h1>
-                <input type="text" className='mx-16 mt-1 mb-2 rounded-2xl bg-white p-3 font-bold' />
+                <input
+                type="text"
+                className="mx-16 mt-1 mb-2 rounded-2xl bg-white p-3 font-bold"
+                />
                 <h1 className="pl-16 pt-1 font-bold">Email</h1>
-                <input type="email" className='mx-16 mt-1 mb-2 rounded-2xl bg-white p-3 font-bold' />
+                <input
+                type="email"
+                className="mx-16 mt-1 mb-2 rounded-2xl bg-white p-3 font-bold"
+                />
                 <h1 className="pl-16 pt-1 font-bold">Password Key</h1>
                 <div className="flex flex-row mr-16">
-                <input type="text" className='ml-16 mr-3 mt-1 mb-2 w-full rounded-2xl bg-white p-3 font-bold' />
+                <input
+                    type="text"
+                    className="ml-16 mr-3 mt-1 mb-2 w-full rounded-2xl bg-white p-3 font-bold"
+                />
                 <Button className="mt-1 mb-2 rounded-xl bg-black text-white font-semibold p-1 items-center">
-                Generate
+                    Generate
                 </Button>
                 </div>
-                
-                <Link className='mx-16 my-4 rounded-2xl bg-[#0090FA] p-3 text-white border-none active:border-none text-center font-bold' href={'/data_user'} >Simpan</Link>
-                <button onClick={() => setOpenModal(false)} className="text-black">Close</button>
+                <Button
+                className="mx-16 my-4 rounded-2xl bg-[#0090FA] p-3 text-white border-none active:border-none text-center font-bold"
+                onClick={() => setOpenModal(false)}
+                >
+                Simpan
+                </Button>
             </div>
             </Modal.Body>
         </Modal>
